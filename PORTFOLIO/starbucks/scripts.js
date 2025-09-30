4ffc9488573db849bdc04267b65c118b0c952083
Victor Hugo de Evandro Lopes Nascimento <victor.hugo3124@gmail.com>
2025-09-30 14:44:39 -0300

    Add files via upload

---
+++ PORTFOLIO/starbucks/scripts.js
@@ -0,0 +1,22 @@
+let nome = "Victor Hugo"
+let sobrenome = "Lopes"
+console.log(nome)
+
+
+function falemeunome(){
+    console.log(sobrenome)
+}
+
+falemeunome()
+
+
+let circulo = document.querySelector(".circulo")
+
+function trocarCor(cor){
+    circulo.style.background = cor
+    
+}
+
+function trocarImagem(){
+    
+}
